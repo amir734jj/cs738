52a5192b61b9a80085752e96a769f8232d25a324
Amir <hesamian@uwm.edu>
2021-02-17 19:12:52 -0600

    working with sbt

--- hwk2/build.sbt
+++ hwk2/build.sbt
@@ -3,3 +3,5 @@ name := "hw2"
 version := "1.0"
 
 scalaVersion := "2.11.7"
+
+unmanagedBase := baseDirectory.value / "libs"
--- hwk2/src/scala/main/scala/Driver.scala
+++ hwk2/src/scala/main/scala/Driver.scala
@@ -1,20 +1,14 @@
-
-package hwk;
-
-import java.io.File
- 
-
-object Main {
-  def main(args: Array[String]) { 
+object Driver {
+  def main(args: Array[String]) {
     val ast = GenerateAST(new File("test/while.js"))
     ast.prep
-    
+
     print(ast)
-    
+
     ast.visit
-    
+
     println
-    
+
     print(ast.toDotGraph)
 
   }
